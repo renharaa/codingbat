2b186634f3ac666e858ac9e9390ba4d335ed5a79
renharaa <rayn.herrera@gmail.com>
2020-05-10 04:24:35 +0800

    initial commit

--- python/Warmup-1.py
+++ python/Warmup-1.py
@@ -21,7 +21,7 @@ def monkey_trouble(a_smile, b_smile):
 	monkey_trouble(True, False) → False
 	"""
 	
-	return a_smile == b_smile
+	return a_smile is b_smile
 	
 #######################################################################################	
 	
